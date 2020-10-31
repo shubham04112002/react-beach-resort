570e098fd954e8b10456b6ee8d2a1a6641347371
shubham04112002 <shubhamraj0411@gmail.com>
2020-10-31 13:48:16 +0530

    initial commit

--- src/components/Room.js
+++ src/components/Room.js
@@ -7,7 +7,7 @@ export default function Room({room}) {
     return (
         <article className="room">
             <div className="img-container">
-             <img src={images[0] || defaultImage}/>   
+             <img src={images[0] || defaultImage} alt="room"/>   
               <div className="price-top">
                 <h6>${price}</h6>   
                 <p>per night</p>
